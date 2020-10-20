992807487dbec170b8f340ff36f429dd1f55ae60
Restyled.io <commits@restyled.io>
2020-10-20 09:47:01 +0000

    Restyled by clang-format

--- src/layer/interp.cpp
+++ src/layer/interp.cpp
@@ -56,7 +56,7 @@ linear_coeffs(int w, int outw, int* xofs, float* alpha, bool align_corner)
     double scale = (double)w / outw;
     if (align_corner)
     {
-        scale = (double)(w-1) / (outw-1);
+        scale = (double)(w - 1) / (outw - 1);
     }
 
     for (int dx = 0; dx < outw; dx++)
@@ -474,7 +474,7 @@ int Interp::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_
 
     if (bottom_blob.dims == 1)
     {
-        #pragma omp parallel for num_threads(opt.num_threads)
+#pragma omp parallel for num_threads(opt.num_threads)
         for (int q = 0; q < channels; q++)
         {
             Mat top_blob_c = top_blob.channel(q);
@@ -489,7 +489,7 @@ int Interp::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_
         const float hs = outh ? h / (float)outh : 1.f / height_scale;
         const float ws = outw ? w / (float)outw : 1.f / width_scale;
 
-        #pragma omp parallel for num_threads(opt.num_threads)
+#pragma omp parallel for num_threads(opt.num_threads)
         for (int q = 0; q < channels; q++)
         {
             const float* ptr = bottom_blob.channel(q);
@@ -519,7 +519,7 @@ int Interp::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_
         linear_coeffs(w, outw, xofs, alpha, false);
         linear_coeffs(h, outh, yofs, beta, false);
 
-        #pragma omp parallel for num_threads(opt.num_threads)
+#pragma omp parallel for num_threads(opt.num_threads)
         for (int q = 0; q < channels; ++q)
         {
             const Mat src = bottom_blob.channel(q);
@@ -544,7 +544,7 @@ int Interp::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_
         cubic_coeffs(w, outw, xofs, alpha);
         cubic_coeffs(h, outh, yofs, beta);
 
-        #pragma omp parallel for num_threads(opt.num_threads)
+#pragma omp parallel for num_threads(opt.num_threads)
         for (int q = 0; q < channels; q++)
         {
             const Mat src = bottom_blob.channel(q);
@@ -569,7 +569,7 @@ int Interp::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_
         linear_coeffs(w, outw, xofs, alpha, true);
         linear_coeffs(h, outh, yofs, beta, true);
 
-        #pragma omp parallel for num_threads(opt.num_threads)
+#pragma omp parallel for num_threads(opt.num_threads)
         for (int q = 0; q < channels; ++q)
         {
             const Mat src = bottom_blob.channel(q);
