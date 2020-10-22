3e658b0abe214edb271f898752c0837ae1e76235
rainfly <xiechc@gmail.com>
2020-10-22 15:24:24 +0800

    Update arm-linux-gnueabihf.toolchain.cmake

--- toolchains/arm-linux-gnueabihf.toolchain.cmake
+++ toolchains/arm-linux-gnueabihf.toolchain.cmake
@@ -3,7 +3,7 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc")
 set(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++")
-
+set(CMAKE_SYSROOT "/media/user1/V3SSDK_BUILDROOT/v3ssdk/buildroot/out/host/arm-buildroot-linux-gnueabihf/sysroot")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
